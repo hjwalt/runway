1264c1e51dae67ee150fac8fa905d15a24ff9527
Hady Willi <hady.willi@gojek.com>
2023-02-03 07:15:26 +0000

    feat: pointer grouping testing

--- reflect/any_ptr.go
+++ reflect/any_ptr.go
@@ -19,3 +19,23 @@ func GetValue(input any) (any, bool) {
 
 	return reflectedValue.Interface(), true
 }
+
+func IsPointer(input any) bool {
+	if input == nil {
+		return true
+	}
+	if reflect.TypeOf(input).Kind() == reflect.Pointer {
+		return true
+	}
+	return false
+}
+
+func IsNil(input any) bool {
+	if input == nil {
+		return true
+	}
+	if !IsPointer(input) {
+		return false
+	}
+	return reflect.ValueOf(input).IsNil()
+}
